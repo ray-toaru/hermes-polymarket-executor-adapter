56ef592e4169efbe91fd9f1d375e56df773043e1
Ray <ray.toaru@gmail.com>
2026-05-18 17:24:03 +0000

    chore: bump version to 0.25.0

--- src/hermes_polymarket_control/__init__.py
+++ src/hermes_polymarket_control/__init__.py
@@ -1,6 +1,6 @@
 """Hermes control-plane client for the Polymarket execution engine."""
 
-__version__ = "0.24.0"
+__version__ = "0.25.0"
 
 from .client import ExecutorClient
 from .models import TradeIntent, QuantityIntent, Side, MarketRef
